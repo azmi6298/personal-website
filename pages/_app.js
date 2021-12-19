b3ad1fb656b01d23f9392acace5030605bfa74f3
azmi6298 <azmi.filkom@gmail.com>
2021-12-19 20:33:26 +0700

    fix import component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/layout/layout";
+import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
