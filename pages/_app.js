444e24748d3726b29fa668ead46fc231b89f207a
azmi6298 <azmi.filkom@gmail.com>
2021-12-21 21:46:26 +0700

    improve SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 import { ThemeProvider } from "next-themes";
+import * as ga from "../lib/ga";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Layout>
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,22 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <meta name="description" content="Azmi, Web Developer" />
+          <meta
+            name="description"
+            content="Muhammad Azmi, Full Stack Web Developer from Indonesia. Currently using Ruby on Rails and Javascript (Vue, React) on daily basis."
+          />
+          <meta
+            property="og:title"
+            content="Muhammad Azmi, Full Stack Web Developer"
+          />
+          <meta
+            property="og:description"
+            content="Muhammad Azmi, Full Stack Web Developer from Indonesia. Currently using Ruby on Rails and Javascript (Vue, React) on daily basis."
+          />
+          <meta property="og:url" content="https://imzadev.netlify.app" />
+          <meta property="og:type" content="website" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -16,6 +29,22 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
             rel="stylesheet"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
